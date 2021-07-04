a3b7cfcdd9b3d57f4682861f0bc49e9967efd63a
A R <admin@elementalzeal.com>
2021-07-04 13:10:33 -0400

    Refactor loadPattern.js

--- docs/assets/js/loadPatterns.js
+++ docs/assets/js/loadPatterns.js
@@ -39,17 +39,140 @@ function removeProgressBar()
     progressBar.innerHTML = "";
 }
 
+function addPatternRow()
+{
+    const parentColumnDiv = document.createElement("div");
+    parentColumnDiv.classList.add("columns");
+
+    return parentColumnDiv;
+}
+
+function addColumn()
+{
+    const columnDiv = document.createElement("div");
+    columnDiv.classList.add("column");
+
+    return columnDiv;
+}
+
+function addCard()
+{
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    
+    return cardDiv;
+}
+
+function addCardImage()
+{
+    const cardImageDiv = document.createElement("div");
+    cardImageDiv.classList.add("card-image");
+
+    return cardImageDiv;
+}
+
+function addImageFigure()
+{
+    const imageFigure = document.createElement("figure");
+    imageFigure.classList.add("image");
+    imageFigure.classList.add("is-4by3");screen
+    
+    return imageFigure;
+}
+
+function addImage(imageName)
+{
+    const image = document.createElement("img");
+    image.src = "docs/assets/images/" + imageName + ".png"
+
+    return image;
+}
+
+function addCardContentDiv()
+{
+    const cardContentDiv = document.createElement("div");
+    cardContentDiv.classList.add("card-content");
+
+    return cardContentDiv;
+}
+
+function addMediaDiv()
+{
+    const mediaDiv = document.createElement("div");
+    mediaDiv.classList.add("media");
+
+    return mediaDiv;
+}
+
+function addMediaContentDiv()
+{
+    const mediaContentDiv = document.createElement("div");
+    mediaContentDiv.classList.add("media-content");
+
+    return mediaContentDiv;
+}
+
+function addTitle(patternTitle)
+{
+    const title = document.createElement("p");
+    title.classList.add("title");
+    title.classList.add("is-4");
+
+    title.innerText = patternTitle;
+
+    return title;
+}
+
+function addContentDiv()
+{
+    const contentDiv = document.createElement("div");
+    contentDiv.classList.add("content");
+
+    return contentDiv;
+}
+
+function addCardFooter()
+{
+    const cardFooter = document.createElement("footer");
+    cardFooter.classList.add("card-footer");
+
+    return cardFooter;
+}
+
+function addEtsyLink(patternEtsyLink)
+{
+    const etsyLink = document.createElement("a");
+    etsyLink.classList.add("card-footer-item");
+    etsyLink.classList.add("etsy-background");
+
+    etsyLink.innerHTML = "Etsy";
+    etsyLink.href = patternEtsyLink;
+
+    return etsyLink;
+}
+
+function addGumroadLink(patternGumroadLink)
+{
+    const gumroadLink = document.createElement("a");
+    gumroadLink.classList.add("card-footer-item");
+    gumroadLink.classList.add("gumroad-background");
+
+    gumroadLink.innerHTML = "Gumroad";
+    gumroadLink.href = patternGumroadLink;
+
+    return gumroadLink;
+}
+
 function createPatternCard(type) 
 {
     const patternsList = getPatternsList(type);
 
     const patternSection = document.getElementById("patternSection");
-    
-    const parentColumnDiv = document.createElement("div");
-    parentColumnDiv.classList.add("columns");
 
     const numPatterns = patternsList.length;
     const numPatternsPerRow = 3;
+
+    var parentColumnDiv;
 
     for (var i = 0; i < numPatterns; i++)
     {
@@ -57,94 +180,59 @@ function createPatternCard(type)
 
         if (i % numPatternsPerRow == 0)
         {
-            const parentColumnDiv = document.createElement("div");
-            parentColumnDiv.classList.add("columns");
+            parentColumnDiv = addPatternRow();
         }
 
-        const columnDiv = document.createElement("div");
-        columnDiv.classList.add("column");
-
+        const columnDiv = addColumn();
         parentColumnDiv.appendChild(columnDiv);
 
-        const cardDiv = document.createElement("div");
-        cardDiv.classList.add("card");
-
+        const cardDiv = addCard();
         columnDiv.appendChild(cardDiv)
 
-        const cardImageDiv = document.createElement("div");
-        cardImageDiv.classList.add("card-image");
-
+        const cardImageDiv = addCardImage();
         cardDiv.appendChild(cardImageDiv)
 
-        const imageFigure = document.createElement("figure");
-        imageFigure.classList.add("image");
-        imageFigure.classList.add("is-4by3");
-
+        const imageFigure = addImageFigure();
         cardImageDiv.appendChild(imageFigure)
 
-        const image = document.createElement("img");
-        image.src = "docs/assets/images/" + patternsList[i] + ".png"
-
+        const imageName = patternsList[i];
+        const image = addImage(imageName);
         imageFigure.appendChild(image)
 
-        const cardContentDiv = document.createElement("div");
-        cardContentDiv.classList.add("card-content");
-
+        const cardContentDiv = addCardContentDiv();
         cardDiv.appendChild(cardContentDiv);
 
-        const mediaDiv = document.createElement("div");
-        mediaDiv.classList.add("media");
-
+        const mediaDiv = addMediaDiv();
         cardContentDiv.appendChild(mediaDiv);
 
-        const mediaContentDiv = document.createElement("div");
-        mediaContentDiv.classList.add("media-content");
-
+        const mediaContentDiv = addMediaContentDiv();
         mediaDiv.appendChild(mediaContentDiv);
 
-        const titleP = document.createElement("p");
-        titleP.classList.add("title");
-        titleP.classList.add("is-4");
-
-        titleP.innerText = patternInfo.title;
-
-        mediaContentDiv.appendChild(titleP);
+        const patternTitle = patternInfo.title;
+        const title = addTitle(patternTitle);
+        mediaContentDiv.appendChild(title);
         
-        const contentDiv = document.createElement("div");
-        contentDiv.classList.add("content");
+        const contentDiv = addContentDiv();
 
-        contents = "<span class=\"bold\">✦ DMC Floss:</span> " + patternInfo.dmcFloss +  " colours <br>";
+        var contents = "<span class=\"bold\">✦ DMC Floss:</span> " + patternInfo.dmcFloss +  " colours <br>";
         contents += "<span class=\"bold\">✦ Pattern Size:</span> " + patternInfo.patternSize + "<br>";
         contents += "<span class=\"bold\">✦ Completed Size:</span> " + patternInfo.completedSize; 
 
         contentDiv.innerHTML = contents;
-
         cardContentDiv.appendChild(contentDiv);
 
-        const cardFooter = document.createElement("footer");
-        cardFooter.classList.add("card-footer");     
+        const cardFooter = addCardFooter();
     
         if (type == "paid")
         {
-            const etsyLink = document.createElement("a");
-            etsyLink.classList.add("card-footer-item");
-            etsyLink.classList.add("etsy-background");
-
-            etsyLink.innerHTML = "Etsy";
-            etsyLink.href = patternInfo.etsyLink;
-
+            const patternEtsyLink = patternInfo.etsyLink;
+            const etsyLink = addEtsyLink(patternEtsyLink);
             cardFooter.appendChild(etsyLink);
         }
 
-        const gumroadLink = document.createElement("a");
-        gumroadLink.classList.add("card-footer-item");
-        gumroadLink.classList.add("gumroad-background");
-
-        gumroadLink.innerHTML = "Gumroad";
-        gumroadLink.href = patternInfo.gumroadLink;
-
+        const patternGumroadLink = patternInfo.gumroadLink;
+        const gumroadLink = addGumroadLink(patternGumroadLink);
         cardFooter.appendChild(gumroadLink);
-
         cardDiv.appendChild(cardFooter);
 
         patternSection.appendChild(parentColumnDiv);
