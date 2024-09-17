c8eb897c65cb14510b6f07e6e16501a9b45c9c35
A R <infinityBearDesigns@gmail.com>
2024-09-17 14:49:52 -0400

    Add mailing list banner and modal

--- docs/assets/js/utility.js
+++ docs/assets/js/utility.js
@@ -485,3 +485,24 @@ function getArrayInUpperCase(array)
 {
     return array.map(array => array.toUpperCase());
 }
+
+
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
+    .push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
+    n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
+    (window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+    ml('account', '1106331');
+
+function openMailingListModal(shouldBeVisible)
+{
+    const mailingListModal = document.getElementById("mailing-list-modal");
+
+    if (shouldBeVisible)
+    {
+        mailingListModal.classList.add("is-active");
+    }
+    else
+    {
+        mailingListModal.classList.remove("is-active");
+    }
+}
